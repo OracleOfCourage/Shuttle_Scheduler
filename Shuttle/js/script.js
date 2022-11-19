902d4c980f3a30b6f4e80ef82e00d82b60307851
CALoSardo <closardo@yahoo.com>
2022-11-19 14:45:44 -0500

    added ul list to index

--- Shuttle/js/script.js
+++ Shuttle/js/script.js
@@ -8,9 +8,14 @@ function searchButtonClicked()
     //Selects the currently selected Bus Stop
     let selectType = document.querySelector('#location');
 
-if(selectType.value == "skip")
-{
-selectType.value = "";
-}
+// if(selectType.value == "skip")
+// {
+// selectType.value = "";
+// }
     console.log(selectType.value);
+    if(document.querySelector)
+    {
+
+    }
+    document.getElementById("test").append('Test');
 }
