da91eeeeba57f18bb148f9c5f456127979f3416e
CALoSardo <closardo@yahoo.com>
2022-11-19 16:13:01 -0500

    More java code

--- Shuttle/js/script.js
+++ Shuttle/js/script.js
@@ -3,19 +3,61 @@ window.onload = (e) => { document.querySelector("#search").onclick = searchButto
 
 
 
-function searchButtonClicked()
-{
+function searchButtonClicked() {
     //Selects the currently selected Bus Stop
     let selectType = document.querySelector('#location');
 
-// if(selectType.value == "skip")
-// {
-// selectType.value = "";
-// }
+    // if(selectType.value == "skip")
+    // {
+    // selectType.value = "";
+    // }
     console.log(selectType.value);
-    if(document.querySelector)
-    {
 
+    let li = document.createElement("li");
+    // li.setAttribute("id", "listElement")
+    li.innerHTML = "NULL";
+
+    if (selectType.value == "Off Campus") {
+        let busList = [{ id: "listElement", name: "1 Off Campus Express" }, { id: "listElement", name: "1 Off Campus Express" }, { id: "listElement", name: "12 Weekend Retail" }]
+
+        for (let i = 0; i < busList.length; i++) {
+            li.setAttribute("id", busList[i].id);
+            li.innerHTML(busList[i].name);
+        }
+        //     li.innerHTML = "1 Off Campus Express"
+        // document.getElementById("test").append(li);
+
+        // li.innerHTML = "12 Weekend Retail"
+        // document.getElementById("test").append(li);
     }
-    document.getElementById("test").append('Test');
+
+    // if(selectType.value == "Apex")
+    // {
+    //     console.log("overwrite?");
+    // li.innerHTML = "2 Apex"
+    // document.getElementById("test").append(li);
+    // }
+
+    // if(selectType.value == "Apex" || selectType.value == "RIT Inn")
+    // {
+    // li.innerHTML = "3 Apex - RIT Inn"
+    // document.getElementById("test").append(li);
+    //}
+
+    // let loopcheck = false;
+    // console.log(document.querySelector('#test').getElementsByTagName('li').length);
+    // for (let i = 0; i < document.querySelector('#test').getElementsByTagName('li').length; i++) {
+
+    //     if (document.querySelector('#test').getElementsByTagName('li')[i].innerHTML == li.innerHTML) {
+
+    //         loopcheck = true;
+    //     }
+    // }
+    // if(!loopcheck)
+    // {
+    // document.getElementById("test").append(li);
+    // }
+
+    // console.log(document.getElementById("test"));
+
 }
